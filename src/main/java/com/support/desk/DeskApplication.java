e683741bc7d636fa7d89a8d82cd6dff9d52959b4
yashgangu <gangurdeyash122003.com>
2026-06-16 16:00:46 +0530

    Refactor project structure and finalize SupportDesk backend

---
+++ src/main/java/com/support/desk/DeskApplication.java
@@ -0,0 +1,14 @@
+package com.support.desk;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/support/desk/DeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.support.desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
